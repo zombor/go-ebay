16b8ee1dcedcaf0ca2434cf90d416671256c3347
Jeremy Bush <contractfrombelow@gmail.com>
2016-05-31 20:37:53 -0500

    Add RevisionError and ListingEnded error functions

--- errors.go
+++ errors.go
@@ -13,3 +13,23 @@ func (err ebayErrors) Error() string {
 
 	return strings.Join(errors, ",")
 }
+
+func (errs ebayErrors) RevisionError() bool {
+	for _, err := range errs {
+		if err.ErrorCode == 10039 || err.ErrorCode == 10029 || err.ErrorCode == 21916916 {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (errs ebayErrors) ListingEnded() bool {
+	for _, err := range errs {
+		if err.ErrorCode == 291 {
+			return true
+		}
+	}
+
+	return false
+}
